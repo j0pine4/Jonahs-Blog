bb371fa5942ba55ad6ba4d7238ab32a3cf2ad652
Jonah Pine <jpine4@hotmail.com>
2026-01-17 21:28:28 -0500

    Vercel analytics

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,11 @@
 <template>
+    <Analytics />
     <UApp>
         <NuxtPage />
     </UApp>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+
+</script>
